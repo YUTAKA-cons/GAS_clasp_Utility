2b4524f3e348fec989d1017f0a056b1ac1373beb
YUTAKA-cons <64908123+YUTAKA-cons@users.noreply.github.com>
2023-12-13 23:17:16 +0900

    Update clasp_deploy.sh

--- clasp_deploy.sh
+++ clasp_deploy.sh
@@ -14,17 +14,24 @@ if [ $1 ]; then
     cd "$1"
 fi
 
+# copy setting file
+cp appsscript.json ./src
+cp .clasp.json ./src
+
 # get last deployment id
 LAST_DEPLOYMENT_ID=$( clasp deployments | pcregrep -o1 '\- ([A-Za-z0-9\-\_]+) @\d+ - web app meta-version' )
-
 if [ -z "$LAST_DEPLOYMENT_ID" ]; then
     LAST_DEPLOYMENT_ID=$( clasp deployments | tail -1 | pcregrep -o1 '\- ([A-Za-z0-9\-\_]+)' ) 
 fi
 
 # deploy
-if [ $? -gt 0 ]; then
-	clasp deploy --deploymentId $LAST_DEPLOYMENT_ID
-if
+if [ $? -eq 0 ]; then
+    clasp deploy --deploymentId $LAST_DEPLOYMENT_ID
+fi
+
+# delete setting file
+rm ./src/appsscript.json
+rm ./src/.clasp.json
 
 # return
 if [ $1 ]; then
