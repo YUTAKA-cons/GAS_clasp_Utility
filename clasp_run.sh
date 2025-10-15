6b0db7eea84a557263938310f5eb25fd1a86eb77
YUTAKA-cons <64908123+YUTAKA-cons@users.noreply.github.com>
2025-10-15 10:19:54 +0900

    Add files via upload

---
+++ clasp_run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Direstry Action
+if [ $1 ]; then
+    cd "$1"
+fi
+
+# copy setting file
+cp appsscript.json ./src
+cp .clasp.json ./src
+
+# run
+clasp run
+
+# delete setting file
+rm ./src/appsscript.json
+rm ./src/.clasp.json
+
+# return
+if [ $1 ]; then
+    cd ../
+fi
