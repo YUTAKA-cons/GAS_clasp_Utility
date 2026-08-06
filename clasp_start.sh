3cbbe3fce794d589bbd31aaf328682ad9c0dc611
YUTAKA <64908123+YUTAKA-cons@users.noreply.github.com>
2026-08-06 17:04:16 +0900

    update start

--- clasp_start.sh
+++ clasp_start.sh
@@ -6,18 +6,21 @@
 dirName="$1"
 claspType=$2
 pjtName=${dirName#*-}
+if [ -z "$claspType" ]; then 
+    claspType="sheets"
+fi
 
 echo --------------------------------
 echo ${dirName} Project start
 echo
 
 # create directory
-cp -a gas-base ${pjtName}
+cp -na gas-base ${pjtName}
 cd ./${pjtName}
 echo \> Completed copy base files
 
 # clasp start
-clasp create
+clasp create --type=${claspType}
 cd ../
 mv ${pjtName} ${dirName}
 cd ./${dirName}
@@ -32,8 +35,10 @@ rm .clasp.json.bak
 sed -i .bak "s/America\/New_York/Asia\/Tokyo/g" appsscript.json
 rm appsscript.json.bak
 cp appsscript.json appsscript.json.bak
-cat appsscript.json.bak | jq '.+ {"webapp":{"access": "ANYONE_ANONYMOUS","executeAs": "USER_DEPLOYING"}}' > appsscript.json
-rm appsscript.json.bak
+#cat appsscript.json.bak | jq '.+ {"webapp":{"access": "ANYONE_ANONYMOUS","executeAs": "USER_DEPLOYING"}}' > appsscript.json
+#rm appsscript.json.bak
+
+rm -rf src/*
 
 echo 
 echo Compleated !
